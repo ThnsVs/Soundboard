f0a8ebbaab49e3b24543b5eb5daf8f20a8b6aa9e
ThnsVs <ThanosBoukias15@hotmail.com>
2025-06-21 18:40:25 +0300

    feat: Added SFML libraries.
    SFML headers and libraries included and linked correctly. Unused libraries will be cleaned up in the future.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <SFML/Audio.hpp>
 
 int main()
 {
-	std::cout << "Working.." << std::endl;
+	
+	sf::SoundBuffer buffer;
+
+	// Cannot find default sound.
+	if (!buffer.loadFromFile("Sounds/note_do.wav"))
+	{
+		std::cout << "Cannot find default sound." << std::endl;
+		return -1;
+	}
+
+	sf::Sound sound(buffer);
+	sound.play();
+
+	while (sound.getStatus() == sf::Sound::Status::Playing) 
+	{
+		sf::sleep(sf::milliseconds(100));
+	}
 
 	return 0;
 }
